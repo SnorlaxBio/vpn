a5eccb2c24a5c2d9b5b54152f48961027e9ebbdf
snorlax <ceo@snorlax.bio>
2024-05-24 22:06:11 +0900

    Update Address Resolution Protocol Document.

---
+++ src/snorlax/protocol/ethernet/address-resolution-protocol.h
@@ -0,0 +1,13 @@
+/**
+ * @file        snorlax/protocol/ethernet/address-resolution-protocol.h
+ * @brief       
+ * @details
+ * 
+ * @author      snorlax <ceo@snorlax.bio>
+ * @since       May 24, 2024
+ */
+
+#ifndef   __SNORLAX__PROTOCOL_ETHERNET_ADDRESS_RESOLUTION_PROTOCOL__H__
+#define   __SNORLAX__PROTOCOL_ETHERNET_ADDRESS_RESOLUTION_PROTOCOL__H__
+
+#endif // __SNORLAX__PROTOCOL_ETHERNET_ADDRESS_RESOLUTION_PROTOCOL__H__
